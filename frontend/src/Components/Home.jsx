a517536e4d80c4919c2eb110e03721abb0c57a0c
LeonardoMendezR <2108727@ucc.edu.ar>
2024-02-23 15:51:59 -0300

    pagina home SIN css terminada

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -4,20 +4,37 @@ import "./Styles.css";
 const Home = () => {
     return (
         <div className="body-div">
-            <b className="texto-grande">
-                Crea tu página web con nosotros
-            </b>
-            <p>
-                Leos tech es una empresa que se encarga del desarrollo de software y paginas webs para tu empresa
-            </p>
-            <b>
-                Sobre nosotros:
-            </b>
-            <p>
-                Somos estudiantes de ingenieria en sistemas, tenemos conocimientos en:
-                React js, python, golang, etc.
-            </p>
-            
+            <div className="clearfix">
+                <b className="texto-grande">
+                    ¡Construye tu presencia en línea con Leos Tech!
+                </b>
+                <p>
+                    Leos Tech es una empresa dedicada al desarrollo de software y creación de páginas web a medida para empresas como la tuya. Nos especializamos en brindar soluciones digitales que impulsan el crecimiento y la visibilidad de tu negocio en el mundo digital.
+                </p>
+                <p>
+                    Nuestro equipo está formado por programadores con conocimientos en tecnologías de vanguardia como React.js, Python, Golang y mucho más. Nos destacamos por nuestra capacidad para comprender las necesidades específicas de cada cliente y traducirlas en soluciones innovadoras y eficientes.
+                </p>
+                <p>
+                    Al elegir trabajar con Leos Tech, puedes esperar:
+                </p>
+                <ul>
+                    <li>
+                        Desarrollo a medida: Nos adaptamos a las necesidades únicas de tu empresa para ofrecerte soluciones que se ajusten perfectamente a tu visión y objetivos comerciales.
+                    </li>
+                    <li>
+                        Tecnología de última generación: Utilizamos las tecnologías más avanzadas y probadas en el mercado para garantizar que tu sitio web sea moderno, seguro y altamente funcional.
+                    </li>
+                    <li>
+                        Experiencia de usuario excepcional: Nos enfocamos en crear experiencias de usuario intuitivas y atractivas que mantengan a tus clientes comprometidos y vuelvan por más.
+                    </li>
+                    <li>
+                        Soporte continuo: Estamos comprometidos a brindarte un servicio de calidad incluso después del lanzamiento de tu sitio web. Estamos aquí para resolver cualquier consulta o problema que puedas tener y para realizar actualizaciones según sea necesario.
+                    </li>
+                </ul>
+                <p>
+                    En Leos Tech, no solo creamos sitios web, creamos soluciones digitales que impulsan el éxito de tu negocio. ¡Únete a nosotros y lleva tu presencia en línea al siguiente nivel!
+                </p>
+            </div>
         </div>
     );
 };
