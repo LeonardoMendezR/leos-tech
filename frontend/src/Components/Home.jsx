7d77c7fcc26c88d1b9d0a7eaf101e8eba51f1810
LeonardoMendezR <2108727@ucc.edu.ar>
2024-02-23 15:41:36 -0300

    cambios tamaño de letra

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -4,23 +4,20 @@ import "./Styles.css";
 const Home = () => {
     return (
         <div className="body-div">
-            <div className="clearfix">
-            <img src="imagenes/imagen.jpg" style={{ width: '500px', height: 'auto' }} alt="Descripción de la imagen" />
+            <b className="texto-grande">
+                Crea tu página web con nosotros
+            </b>
+            <p>
+                Leos tech es una empresa que se encarga del desarrollo de software y paginas webs para tu empresa
+            </p>
             <b>
-                    Crea tu página web con nosotros
-                </b>
-                <p>
-                    Leos tech es una empresa que se encarga del desarrollo de software y paginas webs para tu empresa
-                </p>
-                <p>
-                    Sobre nosotros:
-                    Somos estudiantes de ingenieria en sistemas, tenemos conocimientos en:
-                    React js, python, golang, etc.
-                </p>
-                <b>
-                    Esta es una prueba pa ver que onda el front, leo no me despidas
-                </b>
-            </div>
+                Sobre nosotros:
+            </b>
+            <p>
+                Somos estudiantes de ingenieria en sistemas, tenemos conocimientos en:
+                React js, python, golang, etc.
+            </p>
+            
         </div>
     );
 };
