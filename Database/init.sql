a5944c5e5ce6c7a9a71ee27e824a07ee040addc2
leomorabito02 <leonardomorabito02@hotmail.com>
2024-03-03 13:56:15 -0300

    update init.sql

--- Database/init.sql
+++ Database/init.sql
@@ -4,13 +4,13 @@ SET time_zone = "+00:00";
 
 USE leosTech;
 
-CREATE TABLE `user`
+CREATE TABLE `users`
 (
     `Id`       bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `Name`     VARCHAR(300) NOT NULL,
     `LastName` VARCHAR(300) NOT NULL,
     `Dni`      VARCHAR(8)   NOT NULL,
-    `Email`    VARCHAR(300), -- Se eliminó 'UNIQUE' de esta línea
+    `Email`    VARCHAR(300) UNIQUE NOT NULL ,
     `Password` VARCHAR(300) NOT NULL,
     `Role`     VARCHAR(10)  NOT NULL
 );
@@ -21,7 +21,7 @@ VALUES ('Leonardo Tomas', 'Mendez Rodriguez', 43998614, 'leomendez@gmail.com',
        ('Leonardo Carmelo', 'Morabito', 44195695, 'leonardomorabito02@gmail.com',
         '$2a$10$EwlJ7rPRJPSpKtpsXchYoOs0.YpG7KAlfr42RmjECDFMelR9ICFQW', 'Admin');
 
-CREATE TABLE portfolio
+CREATE TABLE `portfolios`
 (
     `Id`          bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `Name`        VARCHAR(300) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE portfolio
     `Description` VARCHAR(300) NOT NULL,
     `Link`        VARCHAR(300) NOT NULL
 );
-CREATE TABLE image
+CREATE TABLE `images`
 (
     `Id`          bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `Path`        VARCHAR(300) NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE image
     FOREIGN KEY (PortfolioId) REFERENCES portfolio (Id)
 );
 
-CREATE TABLE servicio
+CREATE TABLE `servicios`
 (
     `Id`       bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `NameTech` VARCHAR(300) NOT NULL,
