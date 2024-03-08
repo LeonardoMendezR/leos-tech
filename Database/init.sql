a2473a7b1073283869debb6ac895a2f00395a6b2
LeonardoMendezR <2108727@ucc.edu.ar>
2024-03-08 17:04:41 -0300

    cambios en la db

--- Database/init.sql
+++ Database/init.sql
@@ -34,8 +34,4 @@ CREATE TABLE IF NOT EXISTS servicios
 );
 
 INSERT INTO users (name, last_name, dni, email, password, role) VALUES
-('Maximo', 'Chattas', '44347116', 'maxichattas@gmail.com', '$2a$10$EwlJ7rPRJPSpKtpsXchYoOs0.YpG7KAlfr42RmjECDFMelR9ICFQW', 'Admin'), -- Password: admin
-('Yago', 'Gandara', '43299061', 'yagogandara@gmail.com', '$2a$10$vnzBIZ0rRDOWX96L/jLhguXPLYG/gAFTbsAMzB/8RtzB5VDuh0jKq', 'Customer'), -- Password: pass
-('Santiago', 'Navas', '41857881', 'santinavas@gmail.com', '$2a$10$vnzBIZ0rRDOWX96L/jLhguXPLYG/gAFTbsAMzB/8RtzB5VDuh0jKq', 'Customer'), -- Password: pass
-('Leonardo Tomas', 'Mendez Rodriguez', '43998614', 'leomendez@gmail.com', '$2a$10$vnzBIZ0rRDOWX96L/jLhguXPLYG/gAFTbsAMzB/8RtzB5VDuh0jKq', 'Customer'); -- Password: pass
-
+('Leonardo Tomas', 'Mendez Rodriguez', '43998614', 'leomendez@gmail.com', '$2a$10$EwlJ7rPRJPSpKtpsXchYoOs0.YpG7KAlfr42RmjECDFMelR9ICFQW', 'Admin');
